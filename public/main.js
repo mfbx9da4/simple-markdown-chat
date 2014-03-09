206b0db9facc980cb0eeae5e315e97608dd06a95
David Adler <dalberto.adler@gmail.com>
2014-03-09 12:23:54 -0300

    added some styles

--- public/main.js
+++ public/main.js
@@ -1,24 +1,63 @@
 var socket = io.connect(host);
 var converter = new Markdown.Converter();
+var chatDisplay = document.querySelector('.chat-display');
+var messageBox = document.querySelector('#message');
+var nameBox = document.querySelector('#name');
+var markdownPreviewBox = document.querySelector('#markdownPreview');
+var sendButton = document.querySelector('#send');
 
-socket.on('message', function(data) {
-    var div = document.createElement('div');
-    var text = data.name + ' : <br>' + data.message;
-    if (document.querySelector('#name').value !== data.name) {
-    	div.classList.add('alert');
-    	div.classList.add('alert-info');
-    }
-    var html = converter.makeHtml(text);
-    div.innerHTML = html;
-    document.querySelector('.chat-display').appendChild(div);
+main();
 
-});
+function main() {
+    listenMessage();
+    listenSendMessage();
+    previewMarkdown();
+}
 
-document.querySelector('#send').addEventListener('click', sendMessage);
+function listenMessage() {
+    socket.on('message', function(data) {
+        var div = document.createElement('div');
+        var text = '<b>' + data.name + ' : </b>' + data.message;
+        var html = converter.makeHtml(text);
+        div.innerHTML = html;
+        chatDisplay.appendChild(div);
+        addStylesToChatMessage(div, data.name);
+
+    });
+}
+
+function listenSendMessage() {
+    sendButton.addEventListener('click', sendMessage);
+    messageBox.value = '';
+}
 
 function sendMessage() {
     socket.emit('message', {
-        message: document.querySelector('#message').value,
-        name: document.querySelector('#name').value
+        message: messageBox.value,
+        name: nameBox.value
     });
+}
+
+function addStylesToChatMessage(div, name) {
+    if (nameBox.value !== name) {
+        div.classList.add('alert');
+        div.classList.add('alert-info');
+        div.classList.add('other');
+    } else {
+        div.classList.add('well');
+        div.classList.add('well-sm');
+        div.classList.add('self');
+    }
+    scrollToBottom();
+}
+
+function scrollToBottom() {
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
+}
+
+function previewMarkdown() {
+    messageBox.addEventListener('input', function (e) {
+        var html = converter.makeHtml(e.target.value);
+        markdownPreviewBox.innerHTML = html;
+    }); 
 }
